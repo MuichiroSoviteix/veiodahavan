2f195b9cef13b3dfe55624f9d21ec8ed96d48909
Eduardo <respawn770@gmail.com>
2020-10-18 17:08:46 -0300

    up

---
+++ commands/clear.js
@@ -0,0 +1,7 @@
+module.exports = async (client,msg) =>{
+    const channel   = msg.channel;
+    const FetchMsg  = await channel.messages.fetch();
+    await channel.bulkDelete(FetchMsg);
+    msg.reply("Chat Limpo!");
+}
+
---
+++ core.js
@@ -0,0 +1,20 @@
+  
+const Discord = require("discord.js");
+const client  = new Discord.Client();
+
+const prefix   = "^";
+const commands = require("./scripts/commandsReader")(prefix);
+
+client.on("ready",()=>{
+    console.log(`Logando com o bot ${client.user.tag}`);
+});
+
+client.on("message",(msg)=>{
+    if(!msg.author.bot){
+        console.log(`${msg.author.username}: ${msg.content}`);
+        const args = msg.content.split(" ");
+        if(commands[args[0]]) commands[args[0]](client,msg);
+    }
+});
+
+client.login("NzY3NDUwNDE5Nzg5NjkyOTI4.X4yF5g.nozwsf7L4FZPY7gbGDrm_-L1Hzs");
